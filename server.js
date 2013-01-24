713919293369b8c98e6db85d2b1199266126242e
Rud <rudolph.sand@gmail.com>
2013-01-24 01:25:07 +0100

    0.1

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ var express = require('express')
   , api = require(__dirname+'/lib/api.js');
 
 
+app.all('*', function(req, res, next){
+  if (!req.get('Origin')) return next();
+  // use "*" here to accept any origin
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  if ('OPTIONS' == req.method) return res.send(200);
+  next();
+});
+
 server.listen(8999);
 
 // express setup
@@ -25,8 +36,8 @@ app.configure(function() {
   app.use(app.router);
   // serve up static file if found
   app.use('/', express.static(__dirname + '/public'));
-  // serve up api routes next
-  api.create(app);
+  // // serve up api routes next
+  // api.create(app);
   // everything else gets index.html
   app.use('/', function (req, res, next) {
     fs.readFile(__dirname + '/public/index.html', 'utf8', function (err, data) {
@@ -35,20 +46,43 @@ app.configure(function() {
   });
 });
 
-// auth reference: http://iamtherockstar.com/blog/2012/02/14/nodejs-and-socketio-authentication-all-way-down/
-io.configure(function() {
-    io.set('authorization', function(data, callback) {
-        var obj = require(__dirname+'/lib/socketsession.js');
-        obj.authorize(app, data, callback);
-        console.log('data._session should be set but is undefined here', data._session)
-    });
+// auth configuration
+// reference: http://iamtherockstar.com/blog/2012/02/14/nodejs-and-socketio-authentication-all-way-down/
+// io.configure(function() {
+//     io.set('authorization', function(data, callback) {
+//         var obj = require(__dirname+'/lib/socketsession.js');
+//         obj.authorize(app, data, callback);
+//     });
+// });
 
-});
+// socket concifugration
+// var publicSocket = io
+//   .configure(function() {
+//       io.set('authorization', function(data, callback) {
+//           var obj = require(__dirname+'/lib/socketsession.js');
+//           obj.authorizePublic(app, data, callback);
+//       });
+//   })
+//   .of('/public')
+//   .on('connection', function (socket) {
+//     var callback = function(data) {
+//       socket.emit('welcome', data);
+//     }
+//   var backend = require(__dirname+'/lib/backend.js');
+//   backend.getCurrentSlide(app, socket, callback);
+// });
+var PublicSocketModel = require(__dirname+'/lib/publicsocket.js');
+var PublicSocket  = PublicSocketModel.create(server, app, io);
+server.addListener('publicSendRefreshOptions', PublicSocket.sendRefreshOptions);
 
-io.sockets.on('connection', function (socket) {
-    var emit = function(slideId) {
-      socket.emit('welcome', { slideId: slideId });
-    }
-  var backend = require(__dirname+'/lib/backend.js');
-  backend.getCurrentSlide(app, socket, emit);
-});
+var DisplaySocketModel = require(__dirname+'/lib/displaysocket.js');
+var DisplaySocket  = DisplaySocketModel.create(server, app, io);
+server.addListener('displaySendRefreshOptions', DisplaySocket.sendRefreshOptions);
+
+var ManagerSocketModel = require(__dirname+'/lib/managersocket.js');
+var ManagerSocket  = ManagerSocketModel.create(server, app, io);
+
+
+
+
+

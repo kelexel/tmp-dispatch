df18a1c0553333c8f6380e13116c6a24a6d74fdd
Rud <rudolph.sand@gmail.com>
2013-07-14 04:17:51 +0200

    optimizations

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 touch ../var/logs/debug.log
 touch ../var/logs/exceptions.log
+
+run(){
+	echo "Running $2 in $1 mode";
+	env NODE_ENV=$1 node $2
+}
+
+
+if [ ! -z $2 ]; then env=$2; else env='dev'; fi
+
 if [ ! -z $1 ] && [ $1 == '-cluster' ]; then
-	env NODE_ENV=dev node ../lib/cluster.js
+	run $env ../lib/cluster.js
 else if [ ! -z $1 ] && [ $1 == '-shell' ]; then
-	env NODE_ENV=dev node ../lib/shell.js
+	run $env ../lib/shell.js
 else
-        env NODE_ENV=dev node ../lib/worker.js
+	run $env ../lib/worker.js
 fi
 fi

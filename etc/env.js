db115162fe894ade12a1076332ae4f3a686c7cb6
Rud <rudolph.sand@gmail.com>
2013-07-14 05:42:57 +0200

    meged envs

--- etc/env.js
+++ etc/env.js
@@ -1,13 +1,27 @@
 var prime = require('prime');
 
- module.exports = _({
-	'backendHostname': 'iconference',
-	'backendPort': 80,
-	'socketUrl': 'http://localhost:8999',
-	'consoleDebugLevel': 'debug',
-	'mongoIP': '172.16.76.130',
-	'mongoPort': '27017',
-	'mongoDB': 'bewiDB',
-	'redisIP': '172.16.76.130',
-	'redisPort': '6379'
-});
+if (process.env.NODE_ENV == 'dev') {
+	 module.exports = _({
+		'backendHostname': 'iconference',
+		'backendPort': 80,
+		'socketUrl': 'http://localhost:8999',
+		'consoleDebugLevel': 'debug',
+		'mongoIP': '172.16.76.130',
+		'mongoPort': '27017',
+		'mongoDB': 'bewiDB',
+		'redisIP': '172.16.76.130',
+		'redisPort': '6379'
+	});
+} else {
+	 module.exports = _({
+		'backendHostname': 'bewi.net',
+		'backendPort': 80,
+		'socketUrl': 'http://10.10.10.2:8999',
+		'consoleDebugLevel': 'debug',
+		'mongoIP': '10.10.10.2',
+		'mongoPort': '27017',
+		'mongoDB': 'bewiDB',
+		'redisIP': '10.10.10.2',
+		'redisPort': '6379'
+	});
+}
